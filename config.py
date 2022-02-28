c278662c5faa03f207442a19a43936d13040c018
touch4645 <k.terazawa@lillyholdings.co.jp>
2022-02-28 16:12:27 +0900

    add another server

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ load_dotenv()
 
 # 環境変数を参照
 import os
-NOTIFIED_TEXT_CHANNEL_ID = os.getenv('NOTIFIED_TEXT_CHANNEL_ID')
+DISCORD_SERVER_ID = os.getenv('DISCORD_SERVER_ID').split(',')
+NOTIFIED_TEXT_CHANNEL_ID = os.getenv('NOTIFIED_TEXT_CHANNEL_ID').split(',')
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-DISCORD_SERVER_ID = os.getenv('DISCORD_SERVER_ID')
--- notifiedBot.py
+++ notifiedBot.py
@@ -15,23 +15,25 @@ async def on_ready():
 # チャンネル入室時の通知処理
 @client.event
 async def on_voice_state_update(member, before, after):
-    # メンバーのギルドが対応サーバーかどうか確認する
-    if member.guild.id == int(config.DISCORD_SERVER_ID):
+    for i in range( len(config.DISCORD_SERVER_ID) ):
 
-        # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
-        text_ch = client.get_channel(int(config.NOTIFIED_TEXT_CHANNEL_ID))
+        # メンバーのギルドが対応サーバーかどうか確認する
+        if member.guild.id == int( config.DISCORD_SERVER_ID[i] ):
 
-        # 入室通知
-        if before.channel is None:
-            msg = f'{member.name} が {after.channel.name} に参加しました。'
-            print(msg)
-            await text_ch.send(msg)
+            # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
+            text_ch = client.get_channel( int( config.NOTIFIED_TEXT_CHANNEL_ID[i] ) )
 
-        # 退室通知
-        if after.channel is None:
-            msg = f'{member.name} が退室しました。'
-            print(msg)
-            await text_ch.send(msg)
+            # 入室通知
+            if before.channel is None:
+                msg = f'{member.name} が {after.channel.name} に参加しました。'
+                print(msg)
+                await text_ch.send(msg)
+
+            # 退室通知
+            if after.channel is None:
+                msg = f'{member.name} が退室しました。'
+                print(msg)
+                await text_ch.send(msg)
 
 # Botのトークンを指定（デベロッパーサイトで確認可能）
 client.run(config.DISCORD_BOT_TOKEN)
